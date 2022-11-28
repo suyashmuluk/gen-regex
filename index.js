cd1833ed9ea7f0bd12f59211212b64249a52f0be
msuyash <suyash.muluk@exportify.in>
2022-11-28 16:40:24 +0530

    Added regex for gst

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function regexpattern(str) {
-    if (str) {
+    if (str && str !== '') {
         if (str === 'mail') {
             return '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$';
         } else if (str === 'pan') {
             return '^[a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}$'
-        } else if(str === 'aadhar') {
+        } else if (str === 'aadhar') {
             return '^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$'
+        } else if (str === 'gst') {
+            return '^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$'
         }
     } else {
-        throw console.error('function regexpattern() requires 1 parameter');
+        console.error('function regexpattern() requires 1 parameter');
     }
-
 }
 
 module.exports = regexpattern;
